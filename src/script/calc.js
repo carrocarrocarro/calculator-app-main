3b3263822e092be0063b80d0c79b437a182b362e
carrocarrocarro <52441064+carrocarrocarro@users.noreply.github.com>
2022-11-27 09:30:59 +0200

    updatet calc

--- src/script/calc.js
+++ src/script/calc.js
@@ -3,22 +3,41 @@ const resetBtn = document.querySelector("#reset");
 const operatorBtns = document.querySelectorAll(".operator");
 const operandBtns = document.querySelectorAll(".operand");
 
-let sum = '';
+let currentSum;
 let num = 0;
 
+let inputOperands = [];
+let calculation = [];
+let num1;
+let num2;
+let operator;
+
+function builCalculation() {
+    console.log(calculation);
+}
+
 operatorBtns.forEach((operatorBtn) => {
+    calculation.push(output.innerHTML);
     operatorBtn.addEventListener("click", (e) => {
+        let number1 = output.innerHTML
         switch(e.target.value) {
             case "+":
+                operator = +
+                calculation.push('+') 
                 console.log('+');
+                builCalculation(calculation)
+                
                 break;
             case "-":
+                calculation.push('-') 
                 console.log('-');
                 break;
             case "*":
+                calculation.push('*') 
                 console.log('*');
                 break;
             case "/":
+                calculation.push('/') 
                 console.log('/');
                 break;
             default:
@@ -30,19 +49,59 @@ operatorBtns.forEach((operatorBtn) => {
 
 operandBtns.forEach((operandBtns) => {
     operandBtns.addEventListener('click', (e) => {
-        if(e.target.value === ".") {
-            num = num + ','
-            return showNum(num)
+
+        if ( e.target.value === "." && inputOperands[inputOperands.length-1] === ".") {
+            console.log("last item .")
+            return
         }
+        else if(e.target.value === "." && inputOperands.includes('.')) {
+            console.log("already contains .")
+            return
+        }
+
+        else if (e.target.value === ".") {
+            inputOperands.push(".")
+            output.innerHTML += "." 
+        } 
+         
+        
+        else if(e.target.value === "del") {
+            inputOperands = []
+            output.innerHTML = "0"
+        } 
+        
+        else if(e.target.value === '0' &&  output.innerHTML === "0") {
+            console.log("empty")
+            return 
+        }
+
+        else {
+            num = e.target.value
+            inputOperands.push(num)
+
+            if(output.innerHTML === "0") {
+                output.innerHTML = num
+            } else {
+            
+            output.innerHTML += num
+            }
+        }
+
+        //console.log(output.innerHTML)
+        // calculation.push(output.innerHTML);
+        
+        //console.log(inputOperands)
+        //console.log("last item is " + inputOperands[inputOperands.length-1])
+        //console.log("current number is "+currentSum)
 
     })
 })
 
-function showNum(num){
-    output.innerHTML = num;
-}
 
-// Checkign if reset btn was clicked
+
+
+
+// Checking if reset btn was clicked
 resetBtn.addEventListener('click', clear);
 // Clears the display
 function clear() {
